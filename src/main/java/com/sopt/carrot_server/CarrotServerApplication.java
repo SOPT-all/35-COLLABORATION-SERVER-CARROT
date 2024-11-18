ec08b17e315adac33d9e4fd0cd8c0c3fdabb5936
dltnals317 <sumin4101@naver.com>
2024-11-18 22:57:22 +0900

    [feat] 프로젝트 초기 설정

---
+++ src/main/java/com/sopt/carrot_server/CarrotServerApplication.java
@@ -0,0 +1,13 @@
+package com.sopt.carrot_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotServerApplication.class, args);
+	}
+
+}
