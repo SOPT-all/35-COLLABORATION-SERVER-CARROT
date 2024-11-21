59a6d166827e2f2f9aa22a33968a618fbe94b3c0
정윤 <sju08227@naver.com>
2024-11-21 23:54:55 +0900

    [refactor] import문 개행 수정

--- src/main/java/com/sopt/carrot_server/app/domain/repository/AddressRepository.java
+++ src/main/java/com/sopt/carrot_server/app/domain/repository/AddressRepository.java
@@ -1,12 +1,8 @@
 package com.sopt.carrot_server.app.domain.repository;
 
 import com.sopt.carrot_server.app.domain.Address;
-import com.sopt.carrot_server.app.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
--- src/main/java/com/sopt/carrot_server/app/domain/repository/ProductRepository.java
+++ src/main/java/com/sopt/carrot_server/app/domain/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.sopt.carrot_server.app.domain.repository;
 
 import com.sopt.carrot_server.app.domain.Product;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
