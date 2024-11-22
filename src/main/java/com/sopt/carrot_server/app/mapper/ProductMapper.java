16eb5c2a1fa7a7c663d73c9873b08e872593226f
dltnals317 <sumin4101@naver.com>
2024-11-22 23:54:47 +0900

    [Fix] 병합 전 충돌 해결

--- src/main/java/com/sopt/carrot_server/app/mapper/ProductMapper.java
+++ src/main/java/com/sopt/carrot_server/app/mapper/ProductMapper.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class ProductMapper {
 
+
     public static SearchProductListResponse toSearchProductListDTO(
             final List<Product> relatedProducts,
             final List<Product> similarProducts
@@ -16,7 +17,9 @@ public class ProductMapper {
                 fromSimilarProducts(similarProducts)
         );
     }
-    public static ProductDetailResponse toProductDetailResponseDTO(Product product){
+
+
+    public static ProductDetailResponse toProductDetailResponseDTO(Product product) {
         return ProductDetailResponse.builder()
                 .productId(product.getId())
                 .productImage(product.getProductImage())
@@ -27,6 +30,8 @@ public class ProductMapper {
                 .view(product.getView())
                 .build();
     }
+
+
     public static List<SearchProductDetailResponse> fromProducts(List<Product> products) {
         return products.stream()
                 .map(product -> SearchProductDetailResponse.of(
@@ -39,6 +44,7 @@ public class ProductMapper {
                 )).toList();
     }
 
+
     public static List<SearchProductDetailResponse> fromSimilarProducts(final List<Product> similarProducts) {
         return similarProducts.stream()
                 .map(product -> SearchProductDetailResponse.of(
@@ -50,6 +56,7 @@ public class ProductMapper {
                         product.getPrice()
                 )).toList();
     }
+
 
     public static UserSellingProductListResponse fromUserSellingProducts(final List<Product> products) {
         return UserSellingProductListResponse.of(products.stream()
